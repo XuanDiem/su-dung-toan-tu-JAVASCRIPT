24ff3de9c04c141cff7b2f211a40a71e4e658d92
xuandiem0011 <xuandiem0011@gmail.com>
2019-03-28 16:02:00 +0700

    add file

---
+++ index1.js
@@ -0,0 +1,10 @@
+
+let width;
+let height;
+let area;
+width = parseInt(prompt("Enter the width"));
+height = parseInt(prompt("Enter the height"));
+area = width* height;
+document.write("Enter width:" + width + "<br>");
+document.write("Enter height:" + height + "<br>");
+document.write("The area is:" + area);
